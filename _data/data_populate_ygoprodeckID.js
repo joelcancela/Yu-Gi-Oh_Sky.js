2f765ac48e25f396bbae622581b7620e6dfb0e36
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2025-03-20 14:45:37 +0100

    Updated database with cardID from ygoprodeck

---
+++ _data/data_populate_ygoprodeckID.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs');
+const data = require('./cardinfo-13598-20-03-25.json');
+
+function main() {
+    console.log("Database length:" + data['data'].length);
+    const cards = data['data'];
+    const sqlGenerated = cards.map((element) => {
+        const id = element['id'];
+        const nameEN = element['name'].replaceAll("'","\\'");
+        return `UPDATE CARDS SET ygoprodeckID = '${id}' WHERE name = '${nameEN}';`
+    }).join('\n');
+    fs.writeFile(__dirname + '/queries.sql', sqlGenerated, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("OK");
+        }
+    });
+}
+
+main();
